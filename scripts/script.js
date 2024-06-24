e56c5cf4790d47701a34685d439c9adca024dbe8
Nicholas1Front <nicholasdeoliveiraeugenio@gmail.com>
2024-06-24 19:52:50 -0300

    24/06/2024 19:51 commit

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,19 @@ const movieName = document.querySelector("#search-input").value  ;
 const API_KEY = '939ac071';
 const searchBtn = document.querySelector("#search-btn");
 
+//elements
+
+const searchInput = document.querySelector("#search-input").value ;
+const searchForResultsContainer = document.querySelector("#search-for-results-container");
+const resultContainer = document.querySelector("#result-container");
+const movieImg = document.querySelector("#movie-img");
+const movieTitle = document.querySelector("#movie-title");
+const movieType = document.querySelector("#movie-type") ;
+const movieRelease = document.querySelector("#movie-release-date") ;
+const movieGender = document.querySelector("#movie-gender") ;
+const moiveRatin = document.querySelector("#movie-rating") ;
+const movieSynopsis = document.querySelector("#movie-synopsis") ;
+
 // async function fetchMovieDetails(movieName , API_KEY) {
 //     const response = await fetch(`https://www.omdbapi.com/?t=${encodeURIComponent(movieName)}&apikey=${API_KEY}`);
 //     const data = await response.json();
@@ -18,4 +31,4 @@ async function getMovie(name,API_KEY){
     return data;
 }
 
-console.log(getMovie("Us",API_KEY));
+// console.log(getMovie("the boys",API_KEY));
